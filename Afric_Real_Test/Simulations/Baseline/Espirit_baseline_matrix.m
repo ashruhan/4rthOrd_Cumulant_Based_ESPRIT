19abfe3879fa1ba8c8c3a65adaea97d28c07d7f8
Andrew <ashruhan@docboxinc.com>
2014-10-21 08:31:37 -0400

    Great stuff used a noise loop in the Espirit matrix, found that high angle with small baseline is good, and low angle with large baseline is good. Will send to Dr. K and Confirm.

--- Afric_Real_Test/Simulations/Baseline/Espirit_baseline_matrix.m
+++ Afric_Real_Test/Simulations/Baseline/Espirit_baseline_matrix.m
@@ -2,20 +2,19 @@
 clear;clc;
 %% Initializations
 g_weight = 1;
-v_weight = 0.1;
+v_weight = 1;
 n_weight = 0;
 g_angle = pi/24;
 v_angle = pi/16;
 array_size = 6;
 Averaged_samples = 100;
 delx=0.5;
-
+baseline=2*delx;
 %% Setting up Random Phi that differ across samples
 
 phi_one = 1i*2*pi*rand(1,Averaged_samples);
 phi_two = 1i*2*pi*rand(1,Averaged_samples);
-pos = 1:array_size;
-pos = pos';
+pos = [1:array_size]';
 distance = 2.*pi.*pos.*delx;  % element separation of 1/2 wavelength
 
 %% Two Element Array size
@@ -26,27 +25,27 @@ X = g_weight.*(exp(-1i*distance*sin(g_angle))*exp(phi_one))... %Ground
     + n_weight.*sqrt(-2.*log(1-rand(array_size,Averaged_samples))).*exp(1i*2*pi*rand(array_size,Averaged_samples));  %Noise
 
 
-Y1 = [X(1,:); X(2,:)]; Y2 = [X(5,:);X(6,:)]; %Baseline = 4d
-baseline=4*delx;
+%S1 = [X(1,:); X(2,:)]; S2 = [X(5,:);X(6,:)]; %Baseline = 4d
+S1 = [X(2,:);X(3,:)]; S2 = [X(4,:);X(5,:)]; %Baseline = 2d
+%S1 = [X(3,:),X(4,:)]; S2 = [X(4,:),X(5,:)]; %Baseline = d
+%S1 = [X(2,:),X(3,:),X(4,:)]; S2 = [X(3,:),X(4,:),X(5,:)]; %Baseline = d
+%S1 = [X(1,:),X(2,:),X(3,:),X(4,:)]; S2 = [X(2,:),X(3,:),X(4,:),X(5,:)]; %Baseline = d
 
-% Y1 = [X(1,2),X(1,3)]; Y2 = [X(1,4),X(1,5)]; %Baseline = 2d
-%    Y1 = [X(1,3),X(1,4)]; Y2 = [X(1,4),X(1,5)]; %Baseline = d
-%     Y1 = [X(1,2),X(1,3),X(1,4)]; Y2 = [X(1,3),X(1,4),X(1,5)]; %Baseline = d
-%     Y1 = [X(1,1),X(1,2),X(1,3),X(1,4)]; Y2 = [X(1,2),X(1,3),X(1,4),X(1,5)]; %Baseline = d
-
-R1 = Y1*Y1';
-R2 = Y1*Y2';
+R1 = S1*S1';
+R2 = S1*S2';
 A =  pinv(R1)*R2;
 
 [u,uv] = eig(A);
 [~,kk]=sort(angle(diag(uv)),'ascend');
 
-phase_one = asin(angle(uv(kk(1),kk(1)))/2/pi/baseline);
-phase_two = asin(angle(uv(kk(2),kk(2)))/2/pi/baseline);
+est_ground_angle = asin(angle(uv(kk(1),kk(1)))/(2*pi*baseline)); 
+est_vegitation_angle = asin(angle(uv(kk(2),kk(2)))/(2*pi*baseline));
 %% Ploting Results
 figure(1)
-plot(phase_one*180/pi,'b+')
+plot(est_ground_angle*180/pi,'bo'); %Estimated ground angle
 hold on;
-plot(phase_two*180/pi,'r+')
+plot(est_vegitation_angle*180/pi,'ro'); %Estimated Vegitation angle
+plot(g_angle*180/pi,'b.'); %actual Ground Phase
+plot(v_angle*180/pi,'r.'); %actual Vegitation phase
 hold off;
 
