f51cb2d4b75272c7d3725fcb15368cfb0e1953c5
Andrew <ashruhan@docboxinc.com>
2014-11-04 07:59:26 -0500

    Yep yep got it to work, the problem was in the sorting routine of the baseline stuff, Looks like we are good to go

--- Afric_Real_Test/Simulations/Baseline/Espirit_baseline_matrix_noiseLoop.m
+++ Afric_Real_Test/Simulations/Baseline/Espirit_baseline_matrix_noiseLoop.m
@@ -9,9 +9,13 @@ g_angle = -pi/24;
 v_angle = pi/16;
 
 delx = 0.5; % Delta x of baseline
-baseline = 2*delx; %Physical baseline of two sample arrays
+baseline = delx; %Physical baseline of two sample arrays
+
+g_phase = sin(g_angle)*2*pi*baseline; %Used in the sorting routine
+v_phase = sin(v_angle)*2*pi*baseline; %Used in the sorting routine
 
 array_size = 6; % the amount of ground samples
+avg_loop_size = 100;
 Averaged_Signal_samples = 20; %Averaged_Signal_samples in matrix
 Noise_samples = 20;
 
@@ -21,7 +25,7 @@ est_vegitation_angle = zeros(1,Noise_samples);
 %% Start of Algorithm
 for Noise_Increment = 1:Noise_samples;
     Noise_W = Noise_Increment*n_weight;
-    for unusedVariable = 1:Averaged_Signal_samples;
+    for unusedVariable = 1:avg_loop_size;
         %% Setting up Random Phi that differ across samples
         phi_one = 1i*2*pi*rand(1,Averaged_Signal_samples);
         phi_two = 1i*2*pi*rand(1,Averaged_Signal_samples);
@@ -37,24 +41,29 @@ for Noise_Increment = 1:Noise_samples;
         %% Setting up Baselines
         % Remember to change the Initial Variable delx if Baseline changes
         
-%         S1 = [X(1,:); X(2,:)]; S2 = [X(5,:);X(6,:)]; %Baseline = 4delx
-%         S1 = [X(2,:);X(3,:)]; S2 = [X(4,:);X(5,:)]; %Baseline = 2delx
+        %         S1 = [X(1,:); X(2,:)]; S2 = [X(5,:);X(6,:)]; %Baseline = 4delx
+        %         S1 = [X(2,:);X(3,:)]; S2 = [X(4,:);X(5,:)]; %Baseline = 2delx
         S1 = [X(3,:);X(4,:)]; S2 = [X(4,:);X(5,:)]; %Baseline = delx
-%         S1 = [X(2,:);X(3,:);X(4,:)]; S2 = [X(3,:);X(4,:);X(5,:)]; %Baseline = delx
-%         S1 = [X(1,:);X(2,:);X(3,:);X(4,:)]; S2 = [X(2,:);X(3,:);X(4,:);X(5,:)]; %Baseline = delx
+        %         S1 = [X(2,:);X(3,:);X(4,:)]; S2 = [X(3,:);X(4,:);X(5,:)]; %Baseline = delx
+        %         S1 = [X(1,:);X(2,:);X(3,:);X(4,:)]; S2 = [X(2,:);X(3,:);X(4,:);X(5,:)]; %Baseline = delx
         
         %% Espirit Algorithm
         R1 = S1*S1';
         R2 = S1*S2';
         A =  pinv(R1)*R2;
         
-        [u,uv] = eig(A);
-        [~,kk]=sort(angle(diag(uv)),'ascend');
-        
         %% Averaging over a matix then averaging that signal in a for loop proportional to the matix rows
         % The Smaller Angle ends up haveing The largest angle Eigen Value
-        est_ground_angle(Noise_Increment) = est_ground_angle(Noise_Increment) + (asin(angle(uv(kk(1),kk(1)))/(2*pi*baseline)))/Averaged_Signal_samples; %Estimated ground angle
-        est_vegitation_angle(Noise_Increment) = est_vegitation_angle(Noise_Increment) + (asin(angle(uv(kk(2),kk(2)))/(2*pi*baseline)))/Averaged_Signal_samples; %Estimated Vegitation angle
+        
+        [u,uv] = eig(A);
+        
+        [~,kk]=sort(abs(angle(diag(uv))-g_phase),'ascend');
+        est_ground_angle(Noise_Increment) = est_ground_angle(Noise_Increment)...
+            + (asin(angle(uv(kk(1),kk(1)))/(2*pi*baseline)))/avg_loop_size; %Estimated ground angle
+        
+        [~,kk]=sort(abs(angle(diag(uv))-v_phase),'ascend');
+        est_vegitation_angle(Noise_Increment) = est_vegitation_angle(Noise_Increment)...
+            + (asin(angle(uv(kk(1),kk(1)))/(2*pi*baseline)))/avg_loop_size; %Estimated Vegitation angle
     end
 end
 
@@ -64,14 +73,14 @@ n_snr = 10*log10(1./(temp*n_weight).^2);
 %% Ploting Results
 g_error = est_ground_angle*180/pi - ones(1,Noise_samples)*g_angle*180/pi;
 v_error = est_vegitation_angle*180/pi - ones(1,Noise_samples)*v_angle*180/pi;
-figure(1);title('Ground in blue Veg in red');
+figure(3);title('Ground in blue Veg in red');
 hold on;
 plot(n_snr,g_error,'b')
 plot(n_snr,v_error,'r')
 xlabel('SNR dB');ylabel('error in degrees')
 hold off;
 
-figure(2);title('Ground in blue Veg in red');
+figure(4);title('Ground in blue Veg in red');
 plot(n_snr,est_ground_angle*180/pi,'bo'); %Estimated ground angle
 hold on;
 plot(n_snr,ones(1,Noise_samples)*g_angle*180/pi,'b.'); %actual Ground Phase
