e7c1b7946b78d2aa52fc612b9376f8eee5a7b90c
Andrew Shruhan <Andrew Shruhan>
2016-02-17 08:00:39 -0500

    found an error in old code that I need to check with DR K

--- Afric_Real_Test/Simulations/ForthOrdSim.m
+++ Afric_Real_Test/Simulations/ForthOrdSim.m
@@ -1,35 +1,35 @@
-%% Afric_Sim_test_ESPRIT
+%% Forth Order ESPRIT
 clc;clear;
 %% Initializations
 alpha = 1; %ground weighting factor
 beta = 1;   %veg weighting factor
-%
-Pol_ground = [1;-1;0]/sqrt(2);
-Pol_Cum_ground = [1;1;0;-1;0;0]/2; %ground
-Pol_vegitation = [1;1;1]/sqrt(3);
-Pol_Cum_vegitation = [1;1;1;1;1;1]/3; %vegitation
+
+Pol_ground = [ 1; 1; 0; -1; 0; 0;]./sqrt(3); %ground
+Pol_vegitation = [ 1; 1; 1; 1; 1; 1;]./sqrt(6); %vegitation
+
 G_O = 30;
-ground_offset = G_O*pi/180; % ground interferomitry offset
-V_O = 35;
-vegitation_offset = V_O*pi/180;    % veg interferomitry offset
-%
-samples = 10;
-Window = 200;    %size of window
-Averaged_samples = 30;
-%
-ground_phase_cum=zeros(Averaged_samples,1); vegitation_phase_cum=zeros(Averaged_samples,1);
-ground_mag_cum=zeros(Averaged_samples,1); vegitation_mag_cum=zeros(Averaged_samples,1);
-ground_phase_esp=zeros(Averaged_samples,1); vegitation_phase_esp=zeros(Averaged_samples,1);
-ground_mag_esp=zeros(Averaged_samples,1); vegitation_mag_esp=zeros(Averaged_samples,1);
-%
-SNR = zeros(1,Averaged_samples);
+ground_offset = G_O*pi/180;
+V_O = 50;
+vegitation_offset = V_O*pi/180;
+
+Averaged_samples = 100;
+Window = 50;    %size of window
+SNR_samples = 30;
+
+ground_phase = zeros(SNR_samples,1); 
+ground_mag = zeros(SNR_samples,1); 
+
+vegitation_phase = zeros(SNR_samples,1);
+vegitation_mag = zeros(SNR_samples,1);
+
+SNR = zeros(1,SNR_samples);
 %% Matrix Construction
-for Averaged_sample = 1:Averaged_samples;
+for SNR_sample = 1:SNR_samples;
     
-    SNR(Averaged_sample)=Averaged_sample-10;
-    Noise = (10^(-SNR(Averaged_sample)/20))/sqrt(3);
+    SNR(SNR_sample)=SNR_sample-10;
+    Noise = (10^(-SNR(SNR_sample)/20))/sqrt(3);
     
-    for unusedvariable = 1:samples
+    for unusedvariable = 1:Averaged_samples
         
         g =  Pol_ground*(sqrt(-2*log(1-rand(1,Window))).*exp(1i*2*pi*rand(1,Window)));
         v =  Pol_vegitation*(sqrt(-2*log(1-rand(1,Window))).*exp(1i*2*pi*rand(1,Window)));
@@ -37,90 +37,81 @@ for Averaged_sample = 1:Averaged_samples;
         s1 = alpha*g + beta*v;
         s2 = alpha*exp(1i*ground_offset)*g + beta*exp(1i*vegitation_offset)*v;
         
-        s1_Noise = s1 + Noise*sqrt(-2*log(1-rand(3,Window))).*exp(1i*2*pi*rand(3,Window));
-        s2_Noise = s2 + Noise*sqrt(-2*log(1-rand(3,Window))).*exp(1i*2*pi*rand(3,Window));
- 
- 
-%% Cumulant Algorithm        
-        p1=[s1_Noise(1,:).*s1_Noise(1,:)
+        s1_Noise = s1 + Noise*sqrt(-2*log(1-rand(6,Window))).*exp(1i*2*pi*rand(6,Window));
+        s2_Noise = s2 + Noise*sqrt(-2*log(1-rand(6,Window))).*exp(1i*2*pi*rand(6,Window));
+        
+        %% ESPRIT Algorithm
+        S1 = [s1_Noise(1,:).*s1_Noise(1,:)
             s1_Noise(2,:).*s1_Noise(2,:)
             s1_Noise(3,:).*s1_Noise(3,:)
             s1_Noise(1,:).*s1_Noise(2,:)
             s1_Noise(1,:).*s1_Noise(3,:)
             s1_Noise(2,:).*s1_Noise(3,:)];
         
-        p2=[s2_Noise(1,:).*s2_Noise(1,:)
+        S2 = [s2_Noise(1,:).*s2_Noise(1,:)
             s2_Noise(2,:).*s2_Noise(2,:)
             s2_Noise(3,:).*s2_Noise(3,:)
             s2_Noise(1,:).*s2_Noise(2,:)
             s2_Noise(1,:).*s2_Noise(3,:)
             s2_Noise(2,:).*s2_Noise(3,:)];
         
-        R11 = p1*p1'/Window;
-        R22 = p1*p2'/Window;
+        R1 = S1*S1'/Window;
+        R2 = S1*S2'/Window;
         
-        A = pinv(R11)*R22;
+        [eigenvec,eigenval] = eig(pinv(R1)*R2);
         
-        [u,c] = eig(A);
+%         [~,srt]=sort(angle(diag(eigenval)),'descend');
         
-        s1 = abs(Pol_Cum_ground'*u);
-        [~,kk] = sort(s1,'descend');
-        ground_phase_cum(Averaged_sample) = ground_phase_cum(Averaged_sample) + 0.5*angle(c(kk(1),kk(1)))/samples;
-        ground_mag_cum(Averaged_sample) = ground_mag_cum(Averaged_sample) + abs(c(kk(1),kk(1)))/samples;
+%         Leig_copol = abs(eigenvec(1,srt(1)))^2 + abs(eigenvec(2,srt(1)))^2;
+%         SLeig_copol = abs(eigenvec(1,srt(2)))^2 + abs(eigenvec(2,srt(2)))^2;
+%                 
+%         if (Leig_copol >= SLeig_copol)
+%             
+%         ground_phase(Averaged_sample) = ground_phase(Averaged_sample) + angle(eigenval(srt(1),srt(1)))/samples;
+%         ground_mag(Averaged_sample) = ground_mag(Averaged_sample) + abs(eigenval(srt(1),srt(1)))/samples;
+%         
+%         vegitation_phase(Averaged_sample) = vegitation_phase(Averaged_sample) + angle(eigenval(srt(2),srt(2)))/samples;
+%         vegitation_mag(Averaged_sample) = vegitation_mag(Averaged_sample) + abs(eigenval(srt(2),srt(2)))/samples;
+%             
+%         else
+%             
+%         ground_phase(Averaged_sample) = ground_phase(Averaged_sample) + 0.5*angle(eigenval(srt(2),srt(2)))/samples;
+%         ground_mag(Averaged_sample) = ground_mag(Averaged_sample) + abs(eigenval(srt(2),srt(2)))/samples;
+%         
+%         vegitation_phase(Averaged_sample) = vegitation_phase(Averaged_sample) + 0.5*angle(eigenval(srt(1),srt(1)))/samples;
+%         vegitation_mag(Averaged_sample) = vegitation_mag(Averaged_sample) + abs(eigenval(srt(1),srt(1)))/samples;
+%             
+%         end
         
-        s2 = abs(Pol_Cum_vegitation'*u);
-        [~,kk] = sort(s2,'descend');
-        vegitation_phase_cum(Averaged_sample) = vegitation_phase_cum(Averaged_sample) + 0.5*angle(c(kk(1),kk(1)))/samples;
-        vegitation_mag_cum(Averaged_sample) = vegitation_mag_cum(Averaged_sample) + abs(c(kk(1),kk(1)))/samples;
-        %% ESPRIT Algorithm
-        p1 = [s1_Noise(1,:)
-            s1_Noise(2,:)
-            s1_Noise(3,:)];
         
-        p2 = [s2_Noise(1,:)
-            s2_Noise(2,:)
-            s2_Noise(3,:)];
+        val1 = abs(Pol_ground'*eigenvec);
+        [~,srt] = sort(val1,'descend');
+        ground_phase(SNR_sample) = ground_phase(SNR_sample) + 0.5*angle(eigenval(srt(1),srt(1)))/Averaged_samples;
+        ground_mag(SNR_sample) = ground_mag(SNR_sample) + abs(eigenval(srt(1),srt(1)))/Averaged_samples;
         
-        R1 = p1*p1'/Window;
-        R2 = p1*p2'/Window;
-        
-        A = pinv(R1)*R2;
-        
-        [u,c] = eig(A);
-        
-        s1 = abs(Pol_ground'*u);
-        [~,kk] = sort(s1,'descend');
-        ground_phase_esp(Averaged_sample) = ground_phase_esp(Averaged_sample) + angle(c(kk(1),kk(1)))/samples;
-        ground_mag_esp(Averaged_sample) = ground_mag_esp(Averaged_sample) + abs(c(kk(1),kk(1)))/samples;
-        
-        s2 = abs(Pol_vegitation'*u);
-        [~,kk] = sort(s2,'descend');
-        vegitation_phase_esp(Averaged_sample) = vegitation_phase_esp(Averaged_sample) + angle(c(kk(1),kk(1)))/samples;
-        vegitation_mag_esp(Averaged_sample) = vegitation_mag_esp(Averaged_sample) + abs(c(kk(1),kk(1)))/samples;
+        val2 = abs(Pol_vegitation'*eigenvec);
+        [~,srt] = sort(val2,'descend');
+        vegitation_phase(SNR_sample) = vegitation_phase(SNR_sample) + 0.5*angle(eigenval(srt(1),srt(1)))/Averaged_samples;
+        vegitation_mag(SNR_sample) = vegitation_mag(SNR_sample) + abs(eigenval(srt(1),srt(1)))/Averaged_samples;
         
     end
 end
 %% Plotting Results
- 
+
 figure(1);
-title('2nd and 4rth Order Modified ESPRIT Interferometric Phases');
+title('4rth Order ESPRIT Interferometric Phases');
 xlabel('SNR dB');ylabel('Int Phase (Degrees)');
 hold on;
-plot(SNR,ground_phase_cum*180/pi,'bx');
-plot(SNR,ground_phase_esp*180/pi,'bo');
-plot(SNR,vegitation_phase_cum*180/pi,'gx');
-plot(SNR,vegitation_phase_esp*180/pi,'go');
-axis([-10 20 -60 -10]);
-legend('4rth Order Ground','2nd Order Ground','4rth Order Vegitation','2nd Order Vegitaion','Location','northeast')
+plot(SNR,ground_phase*180/pi,'bo');
+plot(SNR,vegitation_phase*180/pi,'go');
+legend('4rth Order Ground','4rth Order Vegitaion','Location','northeast')
 hold off
- 
+
 figure(2)
-title('2nd and 4rth Order Modified ESPRIT Coherance');
+title('4rth Order ESPRIT Coherance');
 xlabel('SNR (dB)');ylabel('Maginitude')
 hold on;
-plot(SNR,ground_mag_cum,'bx');
-plot(SNR,ground_mag_esp,'bo');
-plot(SNR,vegitation_mag_cum,'gx');
-plot(SNR,vegitation_mag_esp,'go');
-legend('4rth Order Ground Coherance','2nd Order Ground Coherancee','4rth Order Vegitation Coherance','2nd Order Vegitation Coherancee','Location','southeast');
+plot(SNR,ground_mag,'bo');
+plot(SNR,vegitation_mag,'go');
+legend('4rth Order Ground Coherancee','4rth Order Vegitation Coherancee','Location','southeast');
 hold off;
--- Afric_Real_Test/Simulations/SecondOrdSim.m
+++ Afric_Real_Test/Simulations/SecondOrdSim.m
@@ -1,39 +1,41 @@
-%% Afric_Sim_test_ESPRIT
+%% Second Order ESPRIT
 clc;clear;
 %% Initializations
 alpha = 1; %ground weighting factor
 beta = 1;   %veg weighting factor
-%
-Pol_ground = [1;1;0]/sqrt(2);
-Pol_vegitation = [1;1;1]/sqrt(3);
+
+Pol_ground = [ 1; -1 ; 0;]./sqrt(2);
+Pol_vegitation = [ 1; 1; 1;]./sqrt(3);
 
 G_O = 30;
 ground_offset = G_O*pi/180; % ground interferomitry offset
-V_O = 65;
+V_O = 50;
 vegitation_offset = V_O*pi/180;    % veg interferomitry offset
-%
-samplesperSNR = 200;
-Window = 200;    %size of window
-SNR_samples = 30;
-%
 
-ground_phase=zeros(SNR_samples,1); vegitation_phase=zeros(SNR_samples,1);
-ground_mag=zeros(SNR_samples,1); vegitation_mag=zeros(SNR_samples,1);
-%
+Average_samples = 100;
+Window = 50;    %size of window
+SNR_samples = 30;
+
+ground_phase = zeros(SNR_samples,1); 
+ground_mag = zeros(SNR_samples,1); 
+
+vegitation_phase = zeros(SNR_samples,1);
+vegitation_mag = zeros(SNR_samples,1);
+
 SNR = zeros(1,SNR_samples);
 %% Matrix Construction
-for sample = 1:SNR_samples;
+for SNR_sample = 1:SNR_samples;
     
-    SNR(sample)=sample-10;
-    Noise = (10^(-SNR(sample)/20))/sqrt(3);
+    SNR(SNR_sample)=SNR_sample-10;
+    Noise = (10^(-SNR(SNR_sample)/20))/sqrt(3);
     
-    for unusedvariable = 1:samplesperSNR
+    for unusedvariable = 1:Average_samples
         
-        ground_polNoise =  Pol_ground*(sqrt(-2*log(1-rand(1,Window))).*exp(1i*2*pi*rand(1,Window)));
-        vege_polNoise =  Pol_vegitation*(sqrt(-2*log(1-rand(1,Window))).*exp(1i*2*pi*rand(1,Window)));
+        g =  Pol_ground*(sqrt(-2*log(1-rand(1,Window))).*exp(1i*2*pi*rand(1,Window)));
+        v =  Pol_vegitation*(sqrt(-2*log(1-rand(1,Window))).*exp(1i*2*pi*rand(1,Window)));
         
-        s1 = alpha*ground_polNoise + beta*vege_polNoise;
-        s2 = alpha*exp(1i*ground_offset)*ground_polNoise + beta*exp(1i*vegitation_offset)*vege_polNoise;
+        s1 = alpha*g + beta*v;
+        s2 = alpha*exp(1i*ground_offset)*g + beta*exp(1i*vegitation_offset)*v;
         
         s1_Noise = s1 + Noise*sqrt(-2*log(1-rand(3,Window))).*exp(1i*2*pi*rand(3,Window));
         s2_Noise = s2 + Noise*sqrt(-2*log(1-rand(3,Window))).*exp(1i*2*pi*rand(3,Window));
@@ -50,53 +52,60 @@ for sample = 1:SNR_samples;
         R1 = S1*S1'/Window;
         R2 = S1*S2'/Window;
         
-        [eigenvector,eigenvalue] = eig(pinv(R1)*R2);
+        [eigenvec,eigenval] = eig(pinv(R1)*R2);
         
-        [~,kk] = sort(angle(diag(eigenvalue)),'descend');
-        %
-        %         if (abs(eigenvector(1,kk(1)))^2+abs(eigenvector(2,kk(1)))^2)<...
-        %                 (abs(eigenvector(1,kk(2)))^2+abs(eigenvector(2,kk(2)))^2)
-        %
-        %             ground_phase_esp(sample) = ground_phase_esp(sample) + angle(eigenvalue(kk(1),kk(1)))/samplesperSNR;
-        %             ground_mag_esp(sample) = ground_mag_esp(sample) + abs(eigenvalue(kk(1),kk(1)))/samplesperSNR;
-        %
-        %             vegitation_phase_esp(sample) = vegitation_phase_esp(sample) + angle(eigenvalue(kk(2),kk(2)))/samplesperSNR;
-        %             vegitation_mag_esp(sample) = vegitation_mag_esp(sample) + abs(eigenvalue(kk(2),kk(2)))/samplesperSNR;
-        %
-        %
-        %         elseif (abs(eigenvector(1,kk(2)))^2+abs(eigenvector(2,kk(2)))^2)<...
-        %                 (abs(eigenvector(1,kk(1)))^2+abs(eigenvector(2,kk(1)))^2)
-        %
-        %             ground_phase_esp(sample) = ground_phase_esp(sample) + angle(eigenvalue(kk(2),kk(2)))/samplesperSNR;
-        %             ground_mag_esp(sample) = ground_mag_esp(sample) + abs(eigenvalue(kk(2),kk(2)))/samplesperSNR;
-        %
-        %             vegitation_phase_esp(sample) = vegitation_phase_esp(sample) + angle(eigenvalue(kk(1),kk(1)))/samplesperSNR;
-        %             vegitation_mag_esp(sample) = vegitation_mag_esp(sample) + abs(eigenvalue(kk(1),kk(1)))/samplesperSNR;
-        %         end
-        %
+%         [~,srt]=sort(angle(diag(eigenval)),'descend');
+%         
+%         Leig_copol = abs(eigenvec(1,srt(1)))^2 + abs(eigenvec(2,srt(1)))^2;
+%         SLeig_copol = abs(eigenvec(1,srt(2)))^2 + abs(eigenvec(2,srt(2)))^2;
+%                 
+%         if (Leig_copol >= SLeig_copol)
+%             
+%         ground_phase(Averaged_sample) = ground_phase(Averaged_sample) + angle(eigenval(srt(1),srt(1)))/samples;
+%         ground_mag(Averaged_sample) = ground_mag(Averaged_sample) + abs(eigenval(srt(1),srt(1)))/samples;
+%         
+%         vegitation_phase(Averaged_sample) = vegitation_phase(Averaged_sample) + angle(eigenval(srt(2),srt(2)))/samples;
+%         vegitation_mag(Averaged_sample) = vegitation_mag(Averaged_sample) + abs(eigenval(srt(2),srt(2)))/samples;
+%             
+%         else
+%             
+%         ground_phase(Averaged_sample) = ground_phase(Averaged_sample) + angle(eigenval(srt(2),srt(2)))/samples;
+%         ground_mag(Averaged_sample) = ground_mag(Averaged_sample) + abs(eigenval(srt(2),srt(2)))/samples;
+%         
+%         vegitation_phase(Averaged_sample) = vegitation_phase(Averaged_sample) + angle(eigenval(srt(1),srt(1)))/samples;
+%         vegitation_mag(Averaged_sample) = vegitation_mag(Averaged_sample) + abs(eigenval(srt(1),srt(1)))/samples;
+%             
+%         end
         
-        ground_phase(sample) = ground_phase(sample) + angle(eigenvalue(kk(1),kk(1)))/samplesperSNR;
-        ground_mag(sample) = ground_mag(sample) + abs(eigenvalue(kk(1),kk(1)))/samplesperSNR;
         
-        vegitation_phase(sample) = vegitation_phase(sample) + angle(eigenvalue(kk(2),kk(2)))/samplesperSNR;
-        vegitation_mag(sample) = vegitation_mag(sample) + abs(eigenvalue(kk(2),kk(2)))/samplesperSNR;
+        val1 = abs(Pol_ground'*eigenvec);
+        [~,srt] = sort(val1,'descend');
+        ground_phase(SNR_sample) = ground_phase(SNR_sample) + angle(eigenval(srt(1),srt(1)))/Average_samples;
+        ground_mag(SNR_sample) = ground_mag(SNR_sample) + abs(eigenval(srt(1),srt(1)))/Average_samples;
+        
+        val2 = abs(Pol_vegitation'*eigenvec);
+        [~,srt] = sort(val2,'descend');
+        vegitation_phase(SNR_sample) = vegitation_phase(SNR_sample) + angle(eigenval(srt(1),srt(1)))/Average_samples;
+        vegitation_mag(SNR_sample) = vegitation_mag(SNR_sample) + abs(eigenval(srt(1),srt(1)))/Average_samples;
         
     end
 end
 %% Plotting Results
 
 figure(1);
-title('2nd ESPRIT Interferometric Phases');
+title('2nd Order ESPRIT Interferometric Phases');
 xlabel('SNR dB');ylabel('Int Phase (Degrees)');
 hold on;
-plot(SNR,ground_phase*180/pi,'b');
-plot(SNR,vegitation_phase*180/pi,'g');
+plot(SNR,ground_phase*180/pi,'bo');
+plot(SNR,vegitation_phase*180/pi,'go');
+legend('2nd Order Ground','2nd Order Vegitaion','Location','northeast')
 hold off
 
 figure(2)
 title('2nd Order ESPRIT Coherance');
 xlabel('SNR (dB)');ylabel('Maginitude')
 hold on;
-plot(SNR,ground_mag,'b');
-plot(SNR,vegitation_mag,'g');
+plot(SNR,ground_mag,'bo');
+plot(SNR,vegitation_mag,'go');
+legend('2nd Order Ground Coherancee','2nd Order Vegitation Coherancee','Location','southeast');
 hold off;
